866b63fca641b6dc9071c06d783e6385e484b66d
Victor Paramo <victor.h.paramo.24@gmail.com>
2020-08-14 14:53:55 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-
 class App extends React.Component {  
   constructor(props) {
     super(props);
+    this.state = { count: 0 };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick = () => {
-    
+    this.setState({ count: this.state.count + 1 })
   }
 
   render() {
     return (
       <div>
         <h1>Class Component</h1>
-        <p>You clicked 0 times</p>
+        <p>You clicked {this.state.count} times</p>
         <button onClick={this.handleClick}>
           Click me
         </button>
