35b8dcc2a2fd6c680beb499e208836099cfe3926
Victor Paramo <victor.h.paramo.24@gmail.com>
2020-08-14 12:02:15 -0500

    functional vs class

--- src/App.js
+++ src/App.js
@@ -1,18 +1,62 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const App = () => {  
-  const [clicks, setClicks] = React.useState(0);
-  
-  const handleClick = () => {
-    setClicks((actualState) => actualState + 1);
-  }
-  
+function FunctionalCounter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(`[Functional] You clicked ${count} times`);
+    }, 3000);
+  });
+
   return (
     <div>
-      {clicks}
-      <button onClick={handleClick}>
-        Press me
+      <h1>Functional Component</h1>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
       </button>
+    </div>
+  );
+}
+
+class ClassCounter extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      console.log(`[Class] You clicked ${this.state.count} times`);
+    }, 3000);
+  }
+
+  handleClick = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Class Component</h1>
+        <p>You clicked {this.state.count} times</p>
+        <button onClick={this.handleClick}>
+          Click me
+        </button>
+      </div>
+    )
+  }
+
+}
+
+const App = () => {    
+  return (
+    <div style={{ display: 'flex' }}>
+      <FunctionalCounter />
+      <ClassCounter />
     </div>
   );
 }
