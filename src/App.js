61b2e783f40128b853a529f0d5984fa4c2dcf62f
Victor Paramo <victor.h.paramo.24@gmail.com>
2020-08-14 14:51:41 -0500

    start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
 import React, { useState } from 'react';
 
+// class App extends React.Component {  
+//   constructor(props) {
+//     super(props);
+//     this.state = { count: 0 };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+
+//   handleClick = () => {
+//     this.setState({ count: this.state.count + 1 })
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <h1>Class Component</h1>
+//         <p>You clicked {this.state.count} times</p>
+//         <button onClick={this.handleClick}>
+//           Click me
+//         </button>
+//       </div>
+//     )
+//   }
+// }
+
 const App = () => {  
   const [count, setCount] = useState(0);
 
