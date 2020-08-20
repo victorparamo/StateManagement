067da779d8d1ecb3431640a4bde3edc265875249
Victor Paramo <victor.h.paramo.24@gmail.com>
2020-08-20 11:58:44 -0500

    Initial App.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,3 @@
 import React from 'react';
-class App extends React.Component {  
-  constructor(props) {
-    super(props);
-    this.state = { count: 0 };
-    this.handleClick = this.handleClick.bind(this);
-  }
 
-  handleClick = () => {
-    this.setState({ count: this.state.count + 1 })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Class Component</h1>
-        <p>You clicked {this.state.count} times</p>
-        <button onClick={this.handleClick}>
-          Click me
-        </button>
-      </div>
-    )
-  }
-}
-
-export default App;
+export default () => (<div>App</div>);
